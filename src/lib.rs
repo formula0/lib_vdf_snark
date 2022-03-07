62b136c717dc3430d63b9f6512d7e9f5724fe0ad
AJ Park <ajpark@AJui-MacBookPro.local>
2022-03-07 22:06:31 +0900

    wip with trapdoor

--- src/lib.rs
+++ src/lib.rs
@@ -17,18 +17,44 @@ const TIME_BASE: usize = 2;
 pub struct TrapdoorVDF {
     pub group: RsaGroup,
     pub trapdoor: Integer,
+    pub max_time: Integer,
 }
 
-
-
 impl TrapdoorVDF {
-    pub fn setup(group:&str, modulus: &str) -> Self {
+
+    pub fn setup(group:&str, modulus: &str, t_bits: &str) -> Self {
         let g = RsaGroup::from_strs(group, modulus);
+        let time_base = Integer::from(TIME_BASE);
+        // bits : 2^t (ex. t = 11 , 2^11 = 2048)
+        let time_exp = g.power(&time_base, &Integer::from_str(t_bits).unwrap());
+    
         Self {
             group: g,
-            trapdoor:  Integer::from(1usize)
+            trapdoor: Integer::from(1usize),
+            max_time: time_exp,
         }
     }
+
+    pub fn setup_with_random(g: &str, m_bits: &str, t_bits: &str) -> Self {
+        let modular_size = Integer::from_str(m_bits).unwrap().to_usize().unwrap();
+        let p = Generator::new_prime(modular_size/2);
+        let q = Generator::new_prime(modular_size/2);
+
+        let N = p.clone() * q.clone();
+        let totient = (p.clone() - 1usize) * (q.clone() - 1usize);
+
+        let group = RsaGroup::from_strs(g, N.to_string().as_str());
+
+        let time_base = Integer::from(TIME_BASE);
+        let time_exp = group.power(&time_base, &Integer::from_str(t_bits).unwrap());
+
+        Self{
+            group: group,
+            trapdoor: Integer::from_str(totient.to_string().as_str()).unwrap(),
+            max_time: time_exp,
+        }
+    }
+
     fn rsa_exponent<'a, G, I> (g: &G, b: &G::Elem, l: &Integer, xs: I) -> G::Elem
     where
         G: SemiGroup,
@@ -41,94 +67,81 @@ impl TrapdoorVDF {
         }
         acc /= l;
         g.power(b, &acc)
-    } 
-
-    pub fn setup_with_random(g: &str, m_bits: &str) -> Self {
-        let modular_size = Integer::from_str(m_bits).unwrap().to_usize().unwrap();
-        let p = Generator::new_prime(modular_size/2);
-        let q = Generator::new_prime(modular_size/2);
-        
-        let N = p.clone() * q.clone();
-        let totient = (p.clone() - 1usize) * (q.clone() - 1usize);
-
-        let group = RsaGroup::from_strs(g, N.to_string().as_str());
-
-        Self{
-            group: group,
-            trapdoor: Integer::from_str(totient.to_string().as_str()).unwrap(),
-        }
     }
 
     pub fn eval_with_trapdoor(&self,base: &str, time: &str) -> Integer {
 
-        let mut exp_base = Integer::from(TIME_BASE);
-        let x = exp_base.pow(time.parse::<u32>().unwrap()).clone();
-        let exp = self.group.g.clone().pow_mod(&x,&(self.trapdoor)).unwrap();
+        let time_base = Integer::from(TIME_BASE);
+        // bits : 2^t (ex. t = 11 , 2^11 = 2048)
+        let time_exp = self.group.power(&time_base, &Integer::from_str(time).unwrap());
 
+        // 2^bits 
+        // bits should be more than 2048 because this vdf is based on RSA_2048 with effective trapdoor
+        let mut vdf_exp = self.group.power(&self.group.generator(), &time_exp);
+        let trapdoor_vdf_exp = vdf_exp.pow_mod(&Integer::from(1usize), &self.trapdoor).unwrap();
         let xs = vec![
-            exp
+            trapdoor_vdf_exp
         ];
+
         let b = Integer::from_str(base).unwrap();
 
         Self::rsa_exponent(&self.group, &b, &Integer::from(1usize), xs.iter())
+
     }
 
     pub fn eval(&self, base: &str, time: &str) -> Integer {
 
-        let mut exp_base = Integer::from(TIME_BASE);
-        let x = exp_base.pow(time.parse::<u32>().unwrap()).clone();
-        let exp = self.group.g.clone().pow(x.to_u32().unwrap());
+        let time_base = Integer::from(TIME_BASE);
+        // bits : 2^t (ex. t = 11 , 2^11 = 2048)
+        let time_exp = self.group.power(&time_base, &Integer::from_str(time).unwrap());
+        
 
+        // 2^bits 
+        // bits should be more than 2048 because this vdf is based on RSA_2048 with effective trapdoor
+        let mut vdf_exp = self.group.power(&self.group.generator(), &time_exp);
+        let trapdoor_vdf_exp = vdf_exp.pow_mod(&Integer::from(1usize), &self.max_time).unwrap();
         let xs = vec![
-            exp
+            trapdoor_vdf_exp
         ];
+
         let b = Integer::from_str(base).unwrap();
 
         Self::rsa_exponent(&self.group, &b, &Integer::from(1usize), xs.iter())
+
     }
 
 }
 
-// fn rsa_bench<E, H> (
-//     hash: H,
-// ) where
-// E: Engine,
-// H: Hasher<F = E::Fr> + CircuitHasher<E=E>
-// {
-
-// }
-
-// fn rsa_exponent<'a, G, I>(
-//     g: &G,
-//     b: &G::Elem,
-//     l: &Integer,
-//     xs: I,
-// ) -> G::Elem 
-// where 
-// G: SemiGroup,
-// I: Iterator<Item = &'a Integer> 
-// {
-//     // compute 'b^prod(xs) % m
-//     let mut acc = Integer::from(1);
-//     for x in xs {
-//         acc *= x;
-//     }
-//     acc /= l;
-//     g.power(b, &acc)
-// }
-
-
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
+    use std::{str::FromStr, time::Instant};
 
-    use bellman_bignat::group::RsaGroup;
+    use bellman_bignat::group::{RsaGroup, SemiGroup};
     use rug::Integer;
 
-    use crate::{RSA_2048, rsa_exponent};
+    use crate::{RSA_2048};
 
     use num_primes::{Generator, BigUint};
+
+    fn rsa_exponent<'a, G, I>(
+        g: &G,
+        b: &G::Elem,
+        l: &Integer,
+        xs: I,
+    ) -> G::Elem 
+    where 
+    G: SemiGroup,
+    I: Iterator<Item = &'a Integer> 
+    {
+        // compute 'b^prod(xs) % m
+        let mut acc = Integer::from(1);
+        for x in xs {
+            acc *= x;
+        }
+        acc /= l;
+        g.power(b, &acc)
+    }
 
     #[test]
     fn it_works() {
@@ -139,7 +152,10 @@ mod tests {
     #[test]
     fn test_rsa_exponent() {
         let b = Integer::from(2usize);
+        /*
         let l = Integer::from_str("4378779693322314851078464711427904016245509035623856790738093868399234071816590832271409512479149219732517").unwrap();
+        */
+        let l = Integer::from(1usize);
         let xs = vec![
             Integer::from_str("31937553987974094718323624043504205546834586774376973142156746177420677478688763299109194760111447891192360362820159149396249147942612451155969619775305163496407638473777556838684741069061351141275104169798848446335239243312484965159829326775977793454245590125242263267420883094097592918381012308862157981711929572365175824672174089740874967056535954189180093379786870545069569186432812295310881940305587888652601685710785451536880821959636231557861961996647312938583891145806865161362164404798306963474067144506909829836959487322752735917184127271661403524679313392947295519723541385106382901941073514681220701690463").unwrap();
             2
@@ -163,16 +179,21 @@ mod tests {
         let N_str = N.clone().to_string();
         // println!("N : {}", N_str);
 
-        let g = RsaGroup::from_strs("1337", N_str.as_str());
+        //let g = RsaGroup::from_strs("1337", N_str.as_str());
+        let g = RsaGroup::from_strs("2", N_str.as_str());
         let b = Integer::from(2usize);
         //let l = Integer::from_str(Generator::new_prime(3).to_string().as_str()).unwrap();
         let l = Integer::from(1usize);
         let xs = vec![
-            Integer::from_str(Generator::new_prime(2096).to_string().as_str()).unwrap()
+            Integer::from_str(Generator::new_prime(2048).to_string().as_str()).unwrap()
         ];
+        let eval_time = Instant::now();
         let res = rsa_exponent(&g, &b, &l, xs.iter());
+        let eval_duration = eval_time.elapsed();
         println!("xs : {:?}", &xs);
-        println!("res : {}",&res);
+        println!("res [{:?}] : {}", eval_duration ,&res);
+
+        println!("totient : {}", &totient);
 
         let mut mod_xs = Integer::from(1usize);
         for x in xs {
@@ -184,8 +205,10 @@ mod tests {
         let xs_totient = vec![
             mod_xs.clone()
         ];
+        let trapdoor_time = Instant::now();
         let res_mod = rsa_exponent(&g, &b, &l, xs_totient.iter());
-        println!("res_mod : {}", res_mod);
+        let trap_duration = trapdoor_time.elapsed();
+        println!("res_mod [{:?}] : {}", trap_duration, res_mod);
     }
 
     #[test]
@@ -212,6 +235,8 @@ mod tests {
         let res = rsa_exponent(&g, &b, &l, xs.iter());
         println!("xs : {:?}", &xs);
         println!("res : {}",&res);
+
+        println!("totient : {}", &totient);
 
         let mut mod_xs = Integer::from(1usize);
         for x in xs {
--- tests/trapdoor_test.rs
+++ tests/trapdoor_test.rs
@@ -1,28 +1,35 @@
 extern crate vdf_snark;
 
-use std::str::FromStr;
-
-use bellman_bignat::group::RsaGroup;
-use rug::Integer;
-use vdf_snark::TrapdoorVDF;
-
 use crate::vdf_snark::{RSA_2048};
+use std::time::{Duration, Instant};
 
-use num_primes::{Generator, BigUint};
 
 #[test]
 fn test_rsa_exponent() {
 
-    let vdf = vdf_snark::TrapdoorVDF::setup("2", RSA_2048);
-    let res = vdf.eval("2", "10");
+    let vdf = vdf_snark::TrapdoorVDF::setup("2", RSA_2048, "15");
+    let res = vdf.eval("2", "12");
     println!("{}", res);
 }
 
 #[test]
 fn test_generate_rsa_vdf() {
-    let vdf = vdf_snark::TrapdoorVDF::setup_with_random("2", "2048");
-    let res_eval = vdf.eval("2", "10");
-    println!("eval : {}", res_eval);
-    let res_eval_trapdoor = vdf.eval_with_trapdoor("2", "10");
-    println!("eval_trap : {}", res_eval_trapdoor);   
+
+    let setup_time = Instant::now();
+    let vdf = vdf_snark::TrapdoorVDF::setup_with_random("2", "2048", "15");
+    let setup_duration = setup_time.elapsed();
+
+    let setup_eval = Instant::now();
+    let res_eval = vdf.eval("2", "12");
+    let eval_duration = setup_eval.elapsed();
+    println!("eval : {}", res_eval);  
+    
+    let setup_trap = Instant::now();
+    let res_eval_trapdoor = vdf.eval_with_trapdoor("2", "12");
+    let trap_duration = setup_trap.elapsed();
+    println!("eval_trap : {}", res_eval_trapdoor); 
+
+    println!("[Duration] setup:[{:?}], trap:[{:?}], eval:[{:?}]",setup_duration, trap_duration, eval_duration );
+
+
 }
